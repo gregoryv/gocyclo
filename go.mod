7ad02d21e974654cfb95901a75c266ac2241d5b7
Gregory Vincic <g@7de.se>
2019-04-11 23:11:05 +0200

    Check all go files for complexity

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/gregoryv/gocyclo
 
 go 1.12
+
+require github.com/gregoryv/find v0.0.0-20180328085109-1bbc2d70c482
--- qa_test.go
+++ qa_test.go
@@ -1,21 +1,37 @@
 package gocyclo
 
 import (
-	"path/filepath"
+	"container/list"
 	"testing"
+
+	"github.com/gregoryv/find"
 )
 
 func TestComplexity(t *testing.T) {
-	files, err := filepath.Glob("*.go")
+	goFiles, err := find.ByName("*.go", ".")
 	if err != nil {
 		t.Fatal(err)
 	}
-	max := 5
+
+	files := fileNames(goFiles)
+	max := 2
 	result, ok := Assert(files, max)
 	if !ok {
+		t.Errorf("Maximum complexity %v, exceeded", max)
 		for _, l := range result {
 			t.Log(l)
 		}
-		t.Errorf("Exceeded maximum complexity %v", max)
 	}
+}
+
+func fileNames(files *list.List) (names []string) {
+	var i int
+	names = make([]string, files.Len())
+	for e := files.Front(); e != nil; e = e.Next() {
+		if s, ok := e.Value.(string); ok {
+			names[i] = s
+		}
+		i++
+	}
+	return
 }
